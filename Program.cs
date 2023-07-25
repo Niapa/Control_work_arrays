df45ed0f417f2b17fc7954e6af2f443eae0beab9
Niapa <niapa7@yandex.ru>
2023-07-25 21:53:58 +0700

    3-2 commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿string[] array;
-Console.WriteLine("Введите массив данных через запятую: ");
+Console.WriteLine("Введите массив через запятую: ");
 string str = Console.ReadLine()!;
 array = str.Split(',');
 var n = new string [array.Length];
 int i = 0;
-for (int i = 0; i<array.Length)
+foreach (var value in array)
+{
+    (value.Length <= 3)
+    {
+        
+    }
+    {
